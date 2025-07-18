72fe3c6396c510dcbf52b8d596d24a1df326ffcc
Paularamosof <paulaandrearamospinzon@gmail.com>
2025-07-18 16:04:19 -0500

    first commit

---
+++ toDo.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Contador Interactivo</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Contador</h1>
+  <div id="counter">0</div>
+  <button id="increase">Incrementar</button>
+  <button id="decrease">Decrementar</button>
+
+  <script src="script.js"></script>
+</body>
+</html>
